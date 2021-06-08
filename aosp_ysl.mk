33f66e49c3e5bcbb9d99b940f6fafea62da8c82c
sushmit1 <sushmit718@gmail.com>
2021-06-08 08:15:47 +0000

    ysl: Bump to june redfin fp

--- aosp_ysl.mk
+++ aosp_ysl.mk
@@ -52,8 +52,8 @@ ro.ppui.maintainer_name=Sushmit_Rathore
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Build fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ2A.210505.003/7255357 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.210605.005/7349499 release-keys
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
