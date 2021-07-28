29f33ad95aaea6653a3d60b36886783ee95f79da
sushmit1 <sushmit718@gmail.com>
2021-07-28 06:56:16 +0000

    ysl: Bump to july redfin fp

--- corvus_ysl.mk
+++ corvus_ysl.mk
@@ -47,9 +47,8 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Build fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ3A.210605.005/7349499 release-keys
-
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.210705.001/7380771 release-keys
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
